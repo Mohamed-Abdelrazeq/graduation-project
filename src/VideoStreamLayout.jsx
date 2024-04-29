cc162d917d8e6944bf6a5ba42e689223e65fa806
Mohamed Abdelrazek <mohamedabdelrazek824@gmail.com>
2024-04-29 09:13:22 +0300

    video stream

--- src/VideoStreamLayout.jsx
+++ src/VideoStreamLayout.jsx
@@ -5,7 +5,7 @@ function VideoStreamLayout({ videoSources }) {
   return (
     <div className="flex w-full align-middle justify-center">
       <ReactPlayer
-        url={videoSources[0]}
+        url={primaryPlayer}
         width="60vw"
         height="82vh"
         autoPlay
@@ -14,14 +14,14 @@ function VideoStreamLayout({ videoSources }) {
 
       <div className="pl-4 w-3/10 flex flex-col gap-4  align-middle justify-center">
         <ReactPlayer
-          url={videoSources[0]}
+          url={upperPlayer}
           width="30vw"
           height="40vh"
           autoPlay
           controls
         />
         <ReactPlayer
-          url={videoSources[0]}
+          url={lowerPlater}
           width="30vw"
           height="40vh"
           autoPlay
